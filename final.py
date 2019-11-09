b4b07535fc969f04d94ef2f68c0674765fcbcc4c
ShaunakGaiki <56763906+ShaunakGaiki@users.noreply.github.com>
2019-11-09 16:32:50 -0500

    Add files via upload

---
+++ final.py
@@ -0,0 +1,18 @@
+import tkinter
+from tkinter import *
+root=Tk()
+def retrieve_input():
+    inputValue=textBox.get("1.0","end-1c")
+    print(inputValue)
+    waiting = Label(root,text="Searching... This might take a while...")
+    waiting.pack()
+
+prompt = Label(root,text="Enter the electronic product you need")
+prompt.pack()
+textBox=Text(root, height=2, width=100)
+textBox.pack()
+buttonCommit=Button(root, height=1, width=10, text="Search", 
+                    command=lambda: retrieve_input())
+buttonCommit.pack()
+
+mainloop()
