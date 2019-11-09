31a924e442c609d0031f09efe9886fb51d6f01a4
Rishabh Jain <45778610+J-Rishabh@users.noreply.github.com>
2019-11-09 17:48:07 -0500

    Update ezdub.py
    Working search terms ATM = MacBook and iPhone

--- ezdub.py
+++ ezdub.py
@@ -2,28 +2,25 @@ import requests
 from bs4 import BeautifulSoup
 
 
-keyterms = ["s-item__link","standard-type__product_title"]
+keyterms = ["s-item__link","standard-type__product_title","as-links-name more"]
+websitestring = ["","https://www.staples.com","https://www.apple.com"]
 x=0
-staplesstring = "https://www.staples.com"
+
 def linkScan(result, var):
     soup = BeautifulSoup(result.content, 'lxml')
 
     links = soup.find('a',{"class":keyterms[x]})
     #lowercase = links.lower()
 
-    if var in links.text:
-        if x == 0:
-            print(links.attrs['href'])
-        elif x == 1:
-            print(staplesstring + links.attrs['href'])
-
+    if var in links.text:    
+        print(websitestring[x] + links.attrs['href'])
         print()
-
 
 var = input("What are you purchasing? (Case Sensitive) ")
 
 
 print("Searching for '" + var + "'...")
+print("It may take a while...")
 
 print("Links from EBay:")
 print (linkScan(requests.get("https://www.ebay.com/sch/i.html?_nkw=" + var), var))
@@ -33,6 +30,22 @@ print("Links from Staples:")
 print(linkScan(requests.get("https://www.staples.com/" + var + "/directory_" + var), var))
 
 x=2
+print("Links from Apple:")
+print(linkScan(requests.get("https://www.apple.com/us/search/" + var), var))
+
+
+#print("Links from Best Buy:")
+#print(linkScan(requests.get("https://www.bestbuy.com/site/searchpage.jsp?st=" + var), var))
+#
+#https://www.bestbuy.com
+
+
+
+
+#print("Links from Walmart:")
+#print(linkScan(requests.get("https://www.walmart.com/search/?query=" + var), var))
+#product-title-link line-clamp line-clamp-2
+#https://www.walmart.com
 
 #print("Links from Amazon:")
 #print(linkScan(requests.get("https://www.amazon.com/s?k=" + var), var))
@@ -45,3 +58,5 @@ x=2
 
 
 #print(result.status_code)
+
+
